73443ed7fbd37077873f4a5c77045b9736b34cc2
Matt Jordan <matthew.jordan@uk.fujitsu.com>
2017-02-10 11:43:40 +0000

    Initial Version

---
+++ SportsStore/src/SportsStore/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository _repository;
+
+        public ProductController(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ViewResult List() => View(_repository.Products);
+    }
+}

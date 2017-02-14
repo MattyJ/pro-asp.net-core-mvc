5201595ac2f45de9d5099c5e357ca678a85f15bb
JordanM <JordanM@UK060426LT11>
2017-02-14 09:00:38 +0000

    Entity Framework added ; Bootstrap ; Pagination

--- SportsStore/src/SportsStore/Controllers/ProductController.cs
+++ SportsStore/src/SportsStore/Controllers/ProductController.cs
@@ -4,18 +4,33 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models;
+using SportsStore.Models.ViewModels;
 
 namespace SportsStore.Controllers
 {
     public class ProductController : Controller
     {
-        private IProductRepository _repository;
+        private readonly IProductRepository _repository;
+        public int PageSize = 4;
 
         public ProductController(IProductRepository repository)
         {
             _repository = repository;
         }
 
-        public ViewResult List() => View(_repository.Products);
+        public ViewResult List(int page = 1)
+            => View(new ProductsListViewModel
+            {
+                Products = _repository.Products
+                    .OrderBy(p => p.ProductId)
+                    .Skip((page - 1) * PageSize)
+                    .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = _repository.Products.Count()
+                }
+            });
     }
 }

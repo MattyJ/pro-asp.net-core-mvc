001026282330b53be99b4c914911b1a016ad1f77
JordanM <JordanM@UK060426LT11>
2017-02-14 16:37:05 +0000

    Navigation Component added for Category

--- SportsStore/src/SportsStore/Infrastructure/PageLinkTagHelper.cs
+++ SportsStore/src/SportsStore/Infrastructure/PageLinkTagHelper.cs
@@ -28,6 +28,8 @@ namespace SportsStore.Infrastructure
         public PagingInfo PageModel { get; set; }
         public string PageAction { get; set; }
 
+        [HtmlAttributeName(DictionaryAttributePrefix = "page-url-")]
+        public Dictionary<string,object> PageUrlValues { get; set; } = new Dictionary<string, object>();
         public bool PageClassesEnabled { get; set; } = false;
         public string PageClass { get; set; }
         public string PageClassNormal { get; set; }
@@ -40,7 +42,8 @@ namespace SportsStore.Infrastructure
             for (var i = 1; i <= PageModel.TotalPages; i++)
             {
                 var tag = new TagBuilder("a");
-                tag.Attributes["href"] = urlHelper.Action(PageAction, new {page = i});
+                PageUrlValues["page"] = i;
+                tag.Attributes["href"] = urlHelper.Action(PageAction, PageUrlValues);
                 if (PageClassesEnabled)
                 {
                     tag.AddCssClass(PageClass);
--- SportsStore/src/SportsStore/Startup.cs
+++ SportsStore/src/SportsStore/Startup.cs
@@ -42,12 +42,28 @@ namespace SportsStore
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
-                    name: "pagination",
-                    template: "Products/Page{page}",
-                    defaults: new {Controller = "Product", action = "List"});
+                    name: null,
+                    template: "{category}/Page{page:int}",
+                    defaults: new {controller = "Product", action = "List"});
 
-                routes.MapRoute(name: "default",
-                    template: "{controller=Product}/{action=List}/{id?}");
+                routes.MapRoute(
+                    name: null,
+                    template: "Page{page:int}",
+                    defaults: new {Controller = "Product", action = "List", page=1});
+
+
+                routes.MapRoute(
+                    name: null,
+                    template: "{category}",
+                    defaults: new { Controller = "Product", action = "List", page = 1 });
+
+                routes.MapRoute(
+                    name: null,
+                    template: "",
+                    defaults: new { Controller = "Product", action = "List", page = 1 });
+
+                routes.MapRoute(name: null,
+                    template: "{controller}/{action}/{id?}");
             });
             SeedData.EnsurePopulated(app);
         }

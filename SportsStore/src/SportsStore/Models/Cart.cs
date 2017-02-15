02858b5219882fa749dcbc2294a1e5973b44c71d
JordanM <JordanM@UK060426LT11>
2017-02-15 19:42:56 +0000

    Shopping Cart model plus associated unit tests

---
+++ SportsStore/src/SportsStore/Models/Cart.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public class Cart
+    {
+        private readonly List<CartLine> _lineCollection = new List<CartLine>();
+
+        public virtual void AddItem(Product product, int quantity)
+        {
+            var line = _lineCollection
+                .FirstOrDefault(p => p.Product.ProductId == product.ProductId);
+
+            if (line == null)
+            {
+                _lineCollection.Add(new CartLine
+                {
+                    Product = product,
+                    Quantity = quantity
+                });
+            }
+            else
+            {
+                line.Quantity += quantity;
+            }
+        }
+
+        public virtual void RemoveLine(Product product) =>
+            _lineCollection.RemoveAll(l => l.Product.ProductId == product.ProductId);
+
+        public virtual decimal ComputeTotalValue() =>
+            _lineCollection.Sum(e => e.Product.Price * e.Quantity);
+
+        public virtual IEnumerable<CartLine> Lines => _lineCollection;
+
+        public virtual void Clear() => _lineCollection.Clear();
+    }
+}
